980632245cd7ff380910b2c2493121610bf9ae6e
moprektr <brianwoer@hotmail.com>
2022-03-21 17:17:31 -0700

    first commit

---
+++ tests/test_fund_me.py
@@ -0,0 +1,30 @@
+from re import M
+from brownie import FundMe, network, config, accounts, MockV3Aggregator, exceptions
+import pytest
+from web3 import Web3
+from scripts.helpful_scripts import get_account
+from scripts.deploy import deploy_fund_me
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+VALUE = 1000000000000000000
+
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
